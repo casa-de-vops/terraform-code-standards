d6c1d7f420fc34625647c119e64d1f8f6413b218
Javier Cevallos <javier.cevallos@microsoft.com>
2024-08-23 06:26:46 -0700

    .

--- .azuredevops/tests/terraform/null.tf
+++ .azuredevops/tests/terraform/null.tf
@@ -1,20 +1,14 @@
 resource "null_resource" "this" {
   # Changes to any instance of the cluster requires re-provisioning
   triggers = {
-    cluster_instance_ids = join(",", aws_instance.cluster[*].id)
-  }
-
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
-  connection {
-    host = element(aws_instance.cluster[*].public_ip, 0)
+    test_kv_version = local.test_kv_version
+    test_eh_version = local.test_eh_version
   }
 
   provisioner "remote-exec" {
-    # Bootstrap script called with private_ip of each node in the cluster
+    # Bootstrap script 
     inline = [
-      "bootstrap-cluster.sh ${join(" ",
-      aws_instance.cluster[*].private_ip)}",
+      "echo 'Hello, World!'",
     ]
   }
 }
